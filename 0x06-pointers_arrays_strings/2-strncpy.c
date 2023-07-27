5beb563cdf28a7eec633ff443a054f1cd5500954
gracie-123 <gracietumbo@gmail.com>
2023-07-27 03:45:31 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 	while (j < n && src[j] != '\o')
 	{
 		dst[j] = src[j];
-		j++
+		j++;
 	}
 	while (j < n)
 	{
 		dest[j] = '\0';
-		j++
+		j++;
 	}
 	return (dest);
 }
