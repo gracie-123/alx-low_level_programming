6e91031319f899f948deb1584ea32ccad37f1246
gracie-123 <gracietumbo@gmail.com>
2023-07-31 06:25:58 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strchr - entry point
+ * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 0 (success) 
+ * Return: Always 0 (success)
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0
+	int i = 0;
 
 	for (; s[i] >= '\0'; i++)
 	{
