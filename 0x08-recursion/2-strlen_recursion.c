67634919a89fb1d880ede8db2694b4182bb1d7de
gracie-123 <gracietumbo@gmail.com>
2023-08-25 10:05:18 -0700

    advanced

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
+
 /**
- *  _strlen_recursion - Returns the length of a string
- *  @s: The string to br measured
- *
- * Return: The length of the string.
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
-	int longit = 0;
-
-	if (*s)
-	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
-	}
-	return (longit);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * factorial - returns the factorial of a number
- * @n: number to return the factorial from
- *
- *  Return: factorial of n
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
+	else if (n == 0)
 		return (1);
-	return (n * factorial(n - 1));
+	else
+		return (n * factorial(n - 1));
 }
